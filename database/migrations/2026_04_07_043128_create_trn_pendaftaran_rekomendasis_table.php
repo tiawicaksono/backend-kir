e5b53edb5120ccef7fb0081163b23228c59205bf
Tia Wicaksono <wicaksono.tia@gmail.com>
2026-05-16 11:30:15 +0700

    migration rekomendasi

--- database/migrations/2026_04_07_043128_create_trn_pendaftaran_rekomendasis_table.php
+++ database/migrations/2026_04_07_043128_create_trn_pendaftaran_rekomendasis_table.php
@@ -20,20 +20,33 @@ return new class extends Migration
             $table->string('no_pemilik_tujuan')->nullable();
             $table->string('nama_pemilik_tujuan')->nullable();
             $table->string('alamat_pemilik_tujuan')->nullable();
-            $table->integer('provinsi_id');
-            $table->foreign('provinsi_id')->references('id')->on('master_provinsis')->restrictOnDelete();
-            $table->integer('kota_id');
-            $table->foreign('kota_id')->references('id')->on('master_kotas')->restrictOnDelete();
-            $table->integer('kecamatan_id');
-            $table->foreign('kecamatan_id')->references('id')->on('master_kecamatans')->restrictOnDelete();
-            $table->integer('kelurahan_id');
-            $table->foreign('kelurahan_id')->references('id')->on('master_kelurahans')->restrictOnDelete();
+
+            $table->foreignId('provinsi_id')
+                ->nullable()
+                ->constrained('master_provinsis')
+                ->restrictOnDelete();
+
+            $table->foreignId('kota_id')
+                ->nullable()
+                ->constrained('master_kotas')
+                ->restrictOnDelete();
+
+            $table->foreignId('kecamatan_id')
+                ->nullable()
+                ->constrained('master_kecamatans')
+                ->restrictOnDelete();
+
+            $table->foreignId('kelurahan_id')
+                ->nullable()
+                ->constrained('master_kelurahans')
+                ->restrictOnDelete();
+
             $table->boolean('is_mutasi_keluar')->default(false);
             $table->boolean('is_numpang_keluar')->default(false);
             $table->integer('area_asal_id')->nullable();
             $table->integer('area_tujuan_id')->nullable();
             $table->boolean('status_sinkron')->default(false);
-            $table->boolean('keterangan_sinkron')->default(false);
+            $table->string('keterangan_sinkron')->nullable();
             $table->timestamps();
         });
     }
